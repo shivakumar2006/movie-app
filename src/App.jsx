2f5076355d64766f35a3ff1b233d9a6b0f0055c9
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-08 13:50:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className='w-full bg-black text-white'>
+            App 
+        </div>
+    )
+}
+
+export default App;
