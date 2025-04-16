9a2770549744e58c6275c382b15d78a62a1e570a
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-16 13:27:12 +0530

    changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  theme: {
-    extend: {}
-  },
+  // theme: {
+  //   extend: {}
+  // },
   plugins: [
     react(),
     tailwindcss(),
